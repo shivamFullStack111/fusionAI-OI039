778a79058b6289f92ca23f87cc8196a3413df49b
shivam <shvam12340987@gmail.com>
2026-05-22 12:34:15 +0530

    change groq ai model with mistral ai model

--- backend/src/config/aiConfig.js
+++ backend/src/config/aiConfig.js
@@ -1,32 +1,15 @@
-// import {
-//   ChatGoogleGenerativeAI,
-//   GoogleGenerativeAIEmbeddings,
-// } from "@langchain/google-genai";
-
-import { ChatGroq } from "@langchain/groq";
+import { ChatMistralAI } from "@langchain/mistralai";
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
 import { CohereEmbeddings } from "@langchain/cohere";
 
 import dotenv from "dotenv";
 dotenv.config();
 
-// export const llm = new ChatGoogleGenerativeAI({
-//   model: "gemini-2.5-flash-lite",
-//   temperature: 0.5,
-//   maxRetries: 2,
-//   apiKey: process.env.GOOGLE_API_KEY,
-// });
-
-// export const embeddingsGenerator = new GoogleGenerativeAIEmbeddings({
-//   model: "gemini-embedding-001",
-//   apiKey: process.env.GOOGLE_API_KEY,
-// });
-
-export const llm = new ChatGroq({
-  model: "llama-3.3-70b-versatile",
+export const llm = new ChatMistralAI({
+  apiKey: process.env.MISTRAL_API_KEY,
+  modelName: "mistral-small",
   temperature: 1,
   maxRetries: 2,
-  apiKey: process.env.GROQ_API_KEY,
 });
 
 export const embeddingsGenerator = new CohereEmbeddings({
